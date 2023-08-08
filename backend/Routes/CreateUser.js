978daa3606f98ff595e3f69779e35993b3008818
Rohit Roy <97011184+dishtam@users.noreply.github.com>
2023-08-08 09:53:47 +0530

    Update CreateUser.js

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 const { body, validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
-const jwtSecret = "MyNameIsRohitRoyAndIAmAtB.Tech4thYear"
+const jwtSecret = JWT_SECRET
 
 router.post(
   '/createuser',
